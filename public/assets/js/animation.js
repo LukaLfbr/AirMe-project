28f479588607d6fbcf6d41003ef2c6903953d5e8
LukaLfbr <luka.lefebvrepro@gmail.com>
2024-08-22 11:25:20 +0200

    Added main cards and stats section.

---
+++ public/assets/js/animation.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let hamMenu = document.querySelector(".ham-menu");
+  let offScreenMenu = document.querySelector(".off-menu");
+
+  hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    offScreenMenu.classList.toggle("active");
+  });
+});
