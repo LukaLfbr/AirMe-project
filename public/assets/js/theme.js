bb910b43e89b036c99cfa816c50effa24dba4aa0
LukaLfbr <luka.lefebvrepro@gmail.com>
2025-02-03 12:03:07 +0100

    Added white logo.

--- public/assets/js/theme.js
+++ public/assets/js/theme.js
@@ -1,12 +1,28 @@
 document.addEventListener("turbo:load", () => {
   const themeToggleButtons = document.querySelectorAll(".theme-toggle");
+  const logo = document.querySelectorAll(".nav-logo");
 
   const toggleTheme = () => {
     const currentTheme = document.documentElement.getAttribute("data-theme");
     const newTheme = currentTheme === "dark" ? "light" : "dark";
     document.documentElement.setAttribute("data-theme", newTheme);
-    document.cookie = `theme=${newTheme}; path=/; max-age=31536000`; // expire dans un an
+    document.cookie = `theme=${newTheme}; path=/; max-age=31536000`;
+    logo.forEach((logo) => {
+      newTheme === "dark"
+        ? (logo.src = "/assets/img/AirMe-logo.png")
+        : (logo.src = "/assets/img/AirMe-logo-white.png");
+    });
   };
+
+  if (document.documentElement.getAttribute("data-theme") === "dark") {
+    logo.forEach((logo) => {
+      logo.src = "/assets/img/AirMe-logo.png";
+    });
+  } else {
+    logo.forEach((logo) => {
+      logo.src = "/assets/img/AirMe-logo-white.png";
+    });
+  }
 
   themeToggleButtons.forEach((button) => {
     button.addEventListener("click", toggleTheme);
