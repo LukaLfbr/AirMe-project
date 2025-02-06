41dafd95b45cc31dcf427b8670b62b744fdd5a63
LukaLfbr <luka.lefebvrepro@gmail.com>
2025-02-06 12:13:10 +0100

    Configuration Platform.sh pour la DB

--- airme_database.sql
+++ airme_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 06 fév. 2025 à 11:36
+-- Généré le : jeu. 06 fév. 2025 à 14:52
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
